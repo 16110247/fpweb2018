879232bca8af873b41d2346af5f36a70ceebe051
16110212 <abdurrahman.sappa@students.amikom.ac.id>
2018-06-27 00:16:11 +0700

    hotproduct

--- hot-products.php
+++ hot-products.php
@@ -8,7 +8,7 @@
           <div class="col-lg-4 col-sm-5"><img src="images/hotproduct/1.png" class="img-responsive img-circle" alt="products">
           </div>
           <div class="col-lg-8 col-sm-7">
-              <h5><a href="property-detail.php">DJI Inspire 2 + Zenmuse X5s</a></h5>
+              <h5><a href="#!/products/product-detail">DJI Inspire 2 + Zenmuse X5s</a></h5>
               <p class="price">Rp 92.000.000,00</p>
           </div>
       </div>
@@ -17,7 +17,7 @@
           <div class="col-lg-4 col-sm-5"><img src="images/hotproduct/2.jpg" class="img-responsive img-circle" alt="products">
           </div>
           <div class="col-lg-8 col-sm-7">
-              <h5><a href="property-detail.php">DJI Matrice 600 + Ronin-MX</a></h5>
+              <h5><a href="#!/products/product-detail">DJI Matrice 600 + Ronin-MX</a></h5>
               <p class="price">Rp 104.000.000,00</p>
           </div>
       </div>
@@ -26,7 +26,7 @@
           <div class="col-lg-4 col-sm-5"><img src="images/hotproduct/3.jpg" class="img-responsive img-circle" alt="products">
           </div>
           <div class="col-lg-8 col-sm-7">
-              <h5><a href="property-detail.php">DJI MAVIC PRO PLATINUM COMBO</a></h5>
+              <h5><a href="#!/products/product-detail">DJI MAVIC PRO PLATINUM COMBO</a></h5>
               <p class="price">Rp 18.800.000,00</p>
           </div>
       </div>
@@ -35,7 +35,7 @@
           <div class="col-lg-4 col-sm-5"><img src="images/hotproduct/4.jpg" class="img-responsive img-circle" alt="products">
           </div>
           <div class="col-lg-8 col-sm-7">
-              <h5><a href="property-detail.php">DJI PHANTOM 4 PRO PLUS OBSIDIAN</a></h5>
+              <h5><a href="#!/products/product-detail">DJI PHANTOM 4 PRO PLUS OBSIDIAN</a></h5>
               <p class="price">Rp 27.000.000,00</p>
           </div>
       </div>
