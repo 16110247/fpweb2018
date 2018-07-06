febcfd7d20f095fc9fdb17a1d1824c26c15d4795
16110247 <evendyx@gmail.com>
2018-07-07 00:12:02 +0700

    all product

--- allproducts.php
+++ allproducts.php
@@ -26,123 +26,123 @@
             </div>
             <div class="row">
             <div class="products-main">
-                <!-- products -->
+               <!-- products -->
                 <div class="col-lg-4 col-sm-6">
                   <div class="products">
                       <div class="image-holder">
-                        <img src="images/properties/1.jpg" class="img-responsive" alt="properties"/>
-                        <div class="status sold">Sold</div>
+                        <img src="images/drone/inspire-1.jpg" class="img-responsive" alt="properties"/>
+                        <div class="status disc">disc 10%</div>
                       </div>
-                      <h4><a href="#!/products/product-detail">Inspire 1</a></h4>
-                      <p class="price">Price: RP.234,900</p>
-                      <a class="btn btn-primary" href="#!/products/product-detail">View Details</a>
+                      <h4><a href="#!/products/inspire-1">Inspire 1</a></h4>
+                      <p class="price">Price: RP.700.000/battery</p>
+                      <a class="btn btn-primary" href="#!/products/inspire-1">View Details</a>
                   </div>
                 </div>
                 <!-- products -->
-                <!-- products -->
+               <!-- products -->
                 <div class="col-lg-4 col-sm-6">
                   <div class="products">
                       <div class="image-holder">
-                        <img src="images/properties/1.jpg" class="img-responsive" alt="properties"/>
-                        <div class="status sold">Sold</div>
+                        <img src="images/drone/inspire-1-pro.jpg" class="img-responsive" alt="properties"/>
+                        <div class="status sold">disc 10%</div>
                       </div>
-                      <h4><a href="/products/product-detail">Royal Inn</a></h4>
-                      <p class="price">Price: $234,900</p>
-                      <a class="btn btn-primary" href="#!/products/product-detail">View Details</a>
+                      <h4><a href="#!/products/inspire-1-pro">Inspire 1 Pro</a></h4>
+                      <p class="price">Price: RP.750.000/battery</p>
+                      <a class="btn btn-primary" href="#!/products/inspire-1-pro">View Details</a>
                   </div>
                 </div>
                 <!-- products -->
-                <!-- products -->
+               <!-- products -->
                 <div class="col-lg-4 col-sm-6">
                   <div class="products">
                       <div class="image-holder">
-                        <img src="images/properties/1.jpg" class="img-responsive" alt="properties"/>
-                        <div class="status sold">Sold</div>
+                        <img src="images/drone/inspire-2.png" class="img-responsive" alt="properties"/>
+                        <div class="status disc">disc 10%</div>
                       </div>
-                      <h4><a href="/products/product-detail">Royal Inn</a></h4>
-                      <p class="price">Price: $234,900</p>
-                      <a class="btn btn-primary" href="#!/products/product-detail">View Details</a>
+                      <h4><a href="#!/products/inspire-2">Inspire 2</a></h4>
+                      <p class="price">Price: RP.800.000/battery</p>
+                      <a class="btn btn-primary" href="#!/products/inspire-2">View Details</a>
                   </div>
                 </div>
-                <!-- products -->
-                <!-- products -->
+                <!-- products --> 
+               <!-- products -->
                 <div class="col-lg-4 col-sm-6">
                   <div class="products">
                       <div class="image-holder">
-                        <img src="images/properties/1.jpg" class="img-responsive" alt="properties"/>
-                        <div class="status sold">Sold</div>
+                        <img src="images/drone/phantom 3 profesional.jpg" class="img-responsive" alt="drone"/>
+                        <div class="status sale">Sale</div>
                       </div>
-                      <h4><a href="/products/product-detail">Royal Inn</a></h4>
-                      <p class="price">Price: $234,900</p>
-                      <a class="btn btn-primary" href="#!/products/product-detail">View Details</a>
+                      <h4><a href="#!/products/phantom-3-pro">Phantom 3 Pro</a></h4>
+                      <p class="price">Price: Rp. 9.000.000</p>
+                      <a class="btn btn-primary" href="#!/products/phantom-3-pro">View Details</a>
                   </div>
                 </div>
                 <!-- products -->
-                <!-- products -->
+               <!-- products -->
                 <div class="col-lg-4 col-sm-6">
                   <div class="products">
                       <div class="image-holder">
-                        <img src="images/properties/1.jpg" class="img-responsive" alt="properties"/>
-                        <div class="status sold">Sold</div>
+                        <img src="images/drone/phantom 3 standard.jpg" class="img-responsive" alt="drone"/>
+                        <div class="status sale">Sale</div>
                       </div>
-                      <h4><a href="/products/product-detail">Royal Inn</a></h4>
-                      <p class="price">Price: $234,900</p>
-                      <a class="btn btn-primary" href="#!/products/product-detail">View Details</a>
+                      <h4><a href="#!/products/phantom-3-standard">Phantom 3 Standard</a></h4>
+                      <p class="price">Price: Rp.6.000.000</p>
+                      <a class="btn btn-primary" href="#!/products/phantom-3-standard">View Details</a>
                   </div>
                 </div>
                 <!-- products -->
-                <!-- products -->
+               <!-- products -->
                 <div class="col-lg-4 col-sm-6">
                   <div class="products">
                       <div class="image-holder">
-                        <img src="images/properties/1.jpg" class="img-responsive" alt="properties"/>
-                        <div class="status sold">Sold</div>
+                        <img src="images/drone/phantom 4 advanced.png" class="img-responsive" alt="drone"/>
+                        <div class="status sale">Sale</div>
                       </div>
-                      <h4><a href="/products/product-detail">Royal Inn</a></h4>
-                      <p class="price">Price: $234,900</p>
-                      <a class="btn btn-primary" href="#!/products/product-detail">View Details</a>
+                      <h4><a href="#!/products/phantom-4-advanced">Phantom 4 Advanced</a></h4>
+                      <p class="price">Price: Rp. 18.000.000</p>
+                      <a class="btn btn-primary" href="#!/products/phantom-4-advanced">View Details</a>
                   </div>
                 </div>
-                <!-- products -->
-                <!-- products -->
+                <!-- products --> 
+               <!-- products -->
                 <div class="col-lg-4 col-sm-6">
                   <div class="products">
                       <div class="image-holder">
-                        <img src="images/properties/1.jpg" class="img-responsive" alt="properties"/>
-                        <div class="status sold">Sold</div>
+                        <img src="images/drone/phantom 3 se.png" class="img-responsive" alt="drone"/>
+                        <div class="status"></div>
                       </div>
-                      <h4><a href="/products/product-detail">Royal Inn</a></h4>
-                      <p class="price">Price: $234,900</p>
-                      <a class="btn btn-primary" href="#!/products/product-detail">View Details</a>
+                      <h4><a href="#!/products/phantom-3-se">Phantom 3 SE</a></h4>
+                      <p class="price">Price: RP.500.000</p>
+                      <a class="btn btn-primary" href="#!/products/phantom-3-se">View Details</a>
                   </div>
                 </div>
                 <!-- products -->
-                <!-- products -->
+               <!-- products -->
                 <div class="col-lg-4 col-sm-6">
                   <div class="products">
                       <div class="image-holder">
-                        <img src="images/properties/1.jpg" class="img-responsive" alt="properties"/>
-                        <div class="status sold">Sold</div>
+                        <img src="images/drone/phantom 4 pro.png" class="img-responsive" alt="drone"/>
+                        <div class="status"></div>
                       </div>
-                      <h4><a href="/products/product-detail">Royal Inn</a></h4>
-                      <p class="price">Price: $234,900</p>
-                      <a class="btn btn-primary" href="#!/products/product-detail">View Details</a>
+                      <h4><a href="#!/products/phantom-4-pro">Phantom 4 Pro</a></h4>
+                      <p class="price">Price: RP 500.000</p>
+                      <a class="btn btn-primary" href="#!/products/phantom-4-pro">View Details</a>
                   </div>
                 </div>
                 <!-- products -->
-                <!-- products -->
+               <!-- products -->
                 <div class="col-lg-4 col-sm-6">
                   <div class="products">
                       <div class="image-holder">
-                        <img src="images/properties/1.jpg" class="img-responsive" alt="properties"/>
-                        <div class="status sold">Sold</div>
+                        <img src="images/drone/phantom 4.png" class="img-responsive" alt="drone"/>
+                        <div class="status"></div>
                       </div>
-                      <h4><a href="/products/product-detail">Royal Inn</a></h4>
-                      <p class="price">Price: $234,900</p>
-                      <a class="btn btn-primary" href="#!/products/product-detail">View Details</a>
+                      <h4><a href="#!/products/phantom-4">Phantom 4</a></h4>
+                      <p class="price">Price: RP 500.000</p>
+                      <a class="btn btn-primary" href="#!/products/phantom-4">View Details</a>
                   </div>
                 </div>
-                <!-- products -->
+                <!-- products --> 
             </div>
             <div class="center">
                 <ul class="pagination">
